501d0fea92bab685f39462820acf670318898ab3
ZephyrTFA <matthew@tfaluc.com>
2024-01-21 14:19:52 -0500

    parse error display

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use std::{
+    fmt::{Display, Formatter},
+    path::PathBuf,
+};
 
 pub mod define_definition;
 pub mod dm_file;
@@ -11,4 +14,15 @@ pub struct ParseError(i32);
 impl ParseError {
     pub const ERROR_DIRECTIVE_PARSE: ParseError = ParseError(1);
     pub const ERROR_FORCED: ParseError = ParseError(2);
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let fail_reason = match self.0 {
+            1 => "Failed to parse directive",
+            2 => "Forced error",
+            _ => "Unknown error",
+        };
+        write!(f, "{}", fail_reason)
+    }
 }
