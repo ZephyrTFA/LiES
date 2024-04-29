5d04caf89b80989630f8805435d5532793110429
ZephyrTFA <matthew@tfaluc.com>
2024-04-29 05:01:25 -0400

    HOLY SHIT WE WORKING

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,4 @@ pub fn main() -> Result<(), String> {
 
     let mut parser = DmParser::new(WORK_DIR);
     parser.load("tgstation.dme")
-    // parser.load(get_test_case())
-}
-
-fn get_test_case() -> PathBuf {
-    let test_case: Vec<&str> = vec![
-        "lua_reference_regex = regex(@\"^((function)|(table)|(thread)|(userdata)): 0x[0-9a-fA-F]+$\")"
-    ];
-
-    let test_case_file = Path::new(&WORK_DIR).join("test_case.dm");
-    println!("TCF: {test_case_file:?}");
-    fs::write(&test_case_file, test_case.join("\n")).expect("failed to write test case file");
-    test_case_file
 }
