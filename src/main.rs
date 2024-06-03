f17aeb2323e481288ad08be261ff59a1b0e4cfc5
ZephyrTFA <matthew@tfaluc.com>
2024-06-03 06:21:32 -0400

    canonicalize error printout

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use dm_parser::lib::DmParser;
 use dotenv::dotenv;
-use log::{error, info};
+use log::{error, info, log, Level};
 use std::env;
-use util::ParseError;
 
 pub mod dm_parser;
 pub mod dm_preprocessor;
@@ -19,14 +18,33 @@ pub fn main() {
     let result = lies();
     stopwatch.stop();
 
-    info!("LiES -- Done in {:.3}s", stopwatch.elapsed().as_secs_f32());
+    log!(
+        if result { Level::Info } else { Level::Error },
+        "LiES -- {} in {:.3}s",
+        if result { "Done" } else { "Failed" },
+        stopwatch.elapsed().as_secs_f32()
+    );
+}
+
+fn lies() -> bool {
+    let game_dir = env::var("GAME_DIR").expect("GAME_DIR not set.");
+    let dme_file = env::var("DME_FILE").expect("DME_FILE not set.");
+    let mut parser = DmParser::new(game_dir);
+    let result = parser.load_path(dme_file);
+
     if result.is_err() {
-        let parse_error = result.err().unwrap();
-        error!("Error: {}", parse_error.to_string());
-        if parse_error.file_path().is_some() {
+        let parse_error = result.as_ref().err().unwrap();
+        error!("Error while parsing:");
+        error!("\t{}", parse_error.to_string());
+        if let Some(file_path) = parse_error.file_path() {
+            let canonical = parser
+                .environment_directory()
+                .join(file_path)
+                .canonicalize()
+                .unwrap();
             error!(
-                "\\- at {}{}",
-                parse_error.file_path().unwrap(),
+                "\tat {}{}",
+                canonical.display(),
                 parse_error
                     .line_number()
                     .map(|num| format!(":{num}"))
@@ -37,12 +55,7 @@ pub fn main() {
         }
     } else {
         info!("Success.");
-    }
-}
+    };
 
-fn lies() -> Result<(), ParseError> {
-    let game_dir = env::var("GAME_DIR").expect("GAME_DIR not set.");
-    let dme_file = env::var("DME_FILE").expect("DME_FILE not set.");
-    let mut parser = DmParser::new(game_dir);
-    parser.load_path(dme_file)
+    result.is_ok()
 }
